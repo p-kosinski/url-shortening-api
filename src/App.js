75a080781587ba634180f476398b3260769e3834
p-kosinski <kosinskipiotr.98@gmail.com>
2022-05-20 23:22:15 +0200

    Add LinkIcon component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import './App.scss';
 
-import LinkButton from './components/common/LinkButton/LinkButton';
-
 const App = () => (
   <div className='App'>
-    <div style={{display: 'grid', justifyItems: 'center'}}>
-      <LinkButton to='#0'>Get Started</LinkButton><br />
-    </div>
-    <LinkButton to='#0' fullWidth>Get Started - fullWidth</LinkButton>
+    
   </div>
 );
 
