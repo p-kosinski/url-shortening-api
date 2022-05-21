1cb9ab7c4fabaf858573c37fc803bc6568459ae8
p-kosinski <kosinskipiotr.98@gmail.com>
2022-05-21 14:54:23 +0200

    Add Container component & global styling rules

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import './App.scss';
 
+import Container from './components/layout/Container/Container';
+
 const App = () => (
-  <div className='App'>
-    
+  <div className='App' style={{
+    width: '100%',
+  }}>
+    <Container>
+      <div style={{
+        display: 'flex',
+        justifyContent: 'space-between'
+      }}>
+        <div
+          style={{
+            height: '2.5rem',
+            width: '2.5rem',
+            backgroundColor: 'black'
+          }}
+        ></div>
+        <div
+          style={{
+            height: '2.5rem',
+            width: '2.5rem',
+            backgroundColor: 'black'
+          }}
+        ></div>
+      </div>
+    </Container>
   </div>
 );
 
---
+++ src/components/layout/Container/Container.js
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+import styles from './Container.module.scss';
+
+const Container = ({ children }) => (
+  <div className={styles.component}>
+    {children}
+  </div>
+);
+
+Container.propTypes = {
+  children: PropTypes.node,
+};
+
+export default Container;
