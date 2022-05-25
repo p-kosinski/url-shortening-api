8f04e1383db123c14ff84181e30d3e1fc443b732
p-kosinski <kosinskipiotr.98@gmail.com>
2022-05-25 17:16:23 +0200

    Fix typos in LinkLogo

--- src/components/common/LinkLogo/LinkLogo.js
+++ src/components/common/LinkLogo/LinkLogo.js
@@ -4,15 +4,15 @@ import { ReactComponent as Logo } from '../../../images/logo.svg';
 
 import styles from './LinkLogo.module.scss';
 
-const LinkHome = ({ to, textColor }) => (
+const LinkLogo = ({ to, textColor }) => (
   <a href={to} className={styles[textColor]}>
     <Logo />
   </a>
 );
 
-LinkHome.propTypes = {
+LinkLogo.propTypes = {
   to: PropTypes.string.isRequired,
-  textColor: PropTypes.oneOf('dark', 'white').isRequired,
+  textColor: PropTypes.oneOf(['dark', 'white']).isRequired,
 };
 
-export default LinkHome;
+export default LinkLogo;
