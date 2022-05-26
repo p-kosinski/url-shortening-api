f2733d8a9ce6402ebdaa2af604d155e53f83a006
p-kosinski <kosinskipiotr.98@gmail.com>
2022-05-26 16:28:49 +0200

    Add header styling for medium and small screens

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import clsx from 'clsx';
+
 import Container from '../Container/Container';
 import LinkLogo from '../../common/LinkLogo/LinkLogo';
 import LinkButton from '../../common/LinkButton/LinkButton';
@@ -7,43 +10,54 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 import styles from './Header.module.scss';
 
-const Header = () => (
-  <header className={styles.component}>
-    <Container>
-      <nav className={styles.nav}>
-        <div className={styles.logoWrapper}>
-          <LinkLogo to='#0' textColor='dark' />
-        </div>
-        <button className={styles.menuBtn}>
-          <FontAwesomeIcon icon={faBars} />
-        </button>
-        <div className={styles.linksWrapper}>
-          <ul>
-            <li>
-              <a href='#0' className={styles.link}>Features</a>
-            </li>
-            <li>
-              <a href='#0' className={styles.link}>Pricing</a>
-            </li>
-            <li>
-              <a href='#0' className={styles.link}>Resources</a>
-            </li>
-          </ul>
-          <hr className={styles.divider} />
-          <ul>
-            <li>
-              <a href='#0' className={styles.link}>Login</a>
-            </li>
-            <li>
-              <LinkButton to='#0' variant='small'>
-                Sign Up
-              </LinkButton>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </Container>
-  </header>
-);
+const Header = () => {
+  const [navOpen, setNavOpen] = useState(false);
+
+  return (
+    <header className={styles.component}>
+      <Container>
+        <nav className={styles.nav}>
+          <div className={styles.logoWrapper}>
+            <LinkLogo to='#0' textColor='dark' />
+          </div>
+          <button
+            className={styles.menuBtn}
+            onClick={() => setNavOpen(!navOpen)}
+          >
+            <FontAwesomeIcon icon={faBars} />
+          </button>
+          <div
+            className={
+              clsx(styles.linksWrapper, navOpen && styles.linksWrapperOpened)
+            }
+          >
+            <ul>
+              <li>
+                <a href='#0' className={styles.link}>Features</a>
+              </li>
+              <li>
+                <a href='#0' className={styles.link}>Pricing</a>
+              </li>
+              <li>
+                <a href='#0' className={styles.link}>Resources</a>
+              </li>
+            </ul>
+            <hr className={styles.divider} />
+            <ul>
+              <li>
+                <a href='#0' className={styles.link}>Login</a>
+              </li>
+              <li>
+                <LinkButton to='#0' variant='small'>
+                  Sign Up
+                </LinkButton>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </Container>
+    </header>
+  );
+};
 
 export default Header;
