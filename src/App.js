17aedb09db02f39f38b7d2fc04fca6e5ca148bfd
p-kosinski <kosinskipiotr.98@gmail.com>
2022-05-31 18:13:28 +0200

    Cleanup App.js code

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
-import { Fragment } from 'react';
-
-import './App.scss';
-
 import Header from './components/layout/Header/Header';
 import Hero from './components/features/Hero/Hero';
-import Container from './components/layout/Container/Container';
+
+import './App.scss';
 
 const App = () => (
   <>
     <Header />
     <main>
       <Hero />
-      <Container>
-        <div style={{
-          backgroundColor: 'black',
-          color: 'white',
-          marginTop: '60px',
-          }}>
-          {[...Array(100)].map((value, i) => {
-            return (
-              <Fragment key={i}>
-                .<br />
-              </Fragment>
-            );
-          })}
-        </div>
-      </Container>
     </main>
   </>
 );
