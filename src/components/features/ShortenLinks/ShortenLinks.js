e08c4358891e64483baf2896c8780f66c2c7cb10
p-kosinski <kosinskipiotr.98@gmail.com>
2022-06-02 20:53:23 +0200

    Add ShortenLink prototype's HTML structure

--- src/components/features/ShortenLinks/ShortenLinks.js
+++ src/components/features/ShortenLinks/ShortenLinks.js
@@ -1,9 +1,66 @@
 import styles from './ShortenLinks.module.scss';
 
 const ShortenLinks = () => (
-  <div className={styles.wrapper}>
-    
-  </div>
+  <section className={styles.component}>
+    <form className={styles.form}>
+      <input
+        placeholder='Shorten a link here...'
+        className={styles.input}
+      />
+      <button className={styles.submitButton}>
+        Shorten it!
+      </button>
+    </form>
+    <div className={styles.links}>
+      <ul>
+        <li>
+          <div className={styles.link}>
+            <div className={styles.originalLink}>
+              <p>https://www.frontendmentor.io</p>
+            </div>
+            <div className={styles.shortenedLink}>
+              <p>
+                <mark>https://rel.ink/k4lKyk</mark>
+              </p>
+            </div>
+            <button className={styles.copyButton}>
+              Copy
+            </button>
+          </div>
+        </li>
+        <li>
+          <div className={styles.link}>
+            <div className={styles.originalLink}>
+              <p>https://www.twitter.com/frontendmentor</p>
+            </div>
+            <div className={styles.shortenedLink}>
+              <p>
+                <mark>https://rel.ink/gxOXp9</mark>
+              </p>
+            </div>
+            <button className={styles.copyButton}>
+              Copy
+            </button>
+          </div>
+        </li>
+        <li>
+          <div className={styles.link}>
+            <div className={styles.originalLink}>
+              <p>https://www.linkedin.com/company/frontend-mentor</p>
+            </div>
+            <div className={styles.shortenedLink}>
+              <p>
+                <mark>https://rel.ink/gob3X9</mark>
+              </p>
+            </div>
+            <button className={styles.copyButton}>
+              Copy
+            </button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section>
 );
 
 export default ShortenLinks;
