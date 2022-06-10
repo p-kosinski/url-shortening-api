1679be4b469366805f3e98f9b978c626cf4472ac
p-kosinski <kosinskipiotr.98@gmail.com>
2022-06-10 17:01:14 +0200

    Add Footer component without content

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from './components/layout/Header/Header';
 import Hero from './components/features/Hero/Hero';
 import MainSection from './components/features/MainSection/MainSection';
 import BoostSection from './components/features/BoostSection/BoostSection';
+import Footer from './components/layout/Footer/Footer';
 
 import './App.scss';
 
@@ -13,6 +14,7 @@ const App = () => (
       <MainSection />
       <BoostSection />
     </main>
+    <Footer />
   </>
 );
 
---
+++ src/components/layout/Footer/Footer.js
@@ -0,0 +1,9 @@
+import styles from './Footer.module.scss';
+
+const Footer = () => (
+  <footer className={styles.component}>
+    
+  </footer>
+);
+
+export default Footer;
