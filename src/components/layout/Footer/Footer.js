214d03bd3ba348f1e833833817f4d7b164186148
p-kosinski <kosinskipiotr.98@gmail.com>
2022-06-10 20:20:42 +0200

    Wrap Footer content in Container

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -1,3 +1,4 @@
+import Container from '../Container/Container';
 import LinkLogo from '../../common/LinkLogo/LinkLogo';
 import LinkIcon from '../../common/LinkIcon/LinkIcon';
 import LinkFooter from '../../common/LinkFooter/LinkFooter';
@@ -11,68 +12,70 @@ import styles from './Footer.module.scss';
 
 const Footer = () => (
   <footer className={styles.component}>
-    <div className={styles.wrapper}>
-      <div className={styles.logoWrapper}>
-        <LinkLogo
-          to='#0'
-          ariaLabel='home page'
-          textColor='white'
-        />
-      </div>
-      <div className={styles.linksWrapper}>
-        <div className={styles.linkList}>
-          <h4>Features</h4>
-          <ul>
-            <li>
-              <LinkFooter to='#0'>Link Shortening</LinkFooter>
-            </li>
-            <li>
-              <LinkFooter to='#0'>Branded Links</LinkFooter>
-            </li>
-            <li>
-              <LinkFooter to='#0'>Analytics</LinkFooter>
-            </li>
-          </ul>
+    <Container>
+      <div className={styles.wrapper}>
+        <div className={styles.logoWrapper}>
+          <LinkLogo
+            to='#0'
+            ariaLabel='home page'
+            textColor='white'
+          />
         </div>
-        <div className={styles.linkList}>
-          <h4>Resources</h4>
-          <ul>
-            <li>
-              <LinkFooter to='#0'>Blog</LinkFooter>
-            </li>
-            <li>
-              <LinkFooter to='#0'>Developers</LinkFooter>
-            </li>
-            <li>
-              <LinkFooter to='#0'>Support</LinkFooter>
-            </li>
-          </ul>
+        <div className={styles.linksWrapper}>
+          <div className={styles.linkList}>
+            <h4>Features</h4>
+            <ul>
+              <li>
+                <LinkFooter to='#0'>Link Shortening</LinkFooter>
+              </li>
+              <li>
+                <LinkFooter to='#0'>Branded Links</LinkFooter>
+              </li>
+              <li>
+                <LinkFooter to='#0'>Analytics</LinkFooter>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.linkList}>
+            <h4>Resources</h4>
+            <ul>
+              <li>
+                <LinkFooter to='#0'>Blog</LinkFooter>
+              </li>
+              <li>
+                <LinkFooter to='#0'>Developers</LinkFooter>
+              </li>
+              <li>
+                <LinkFooter to='#0'>Support</LinkFooter>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.linkList}>
+            <h4>Company</h4>
+            <ul>
+              <li>
+                <LinkFooter to='#0'>About</LinkFooter>
+              </li>
+              <li>
+                <LinkFooter to='#0'>Our Team</LinkFooter>
+              </li>
+              <li>
+                <LinkFooter to='#0'>Careers</LinkFooter>
+              </li>
+              <li>
+                <LinkFooter to='#0'>Contact</LinkFooter>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className={styles.linkList}>
-          <h4>Company</h4>
-          <ul>
-            <li>
-              <LinkFooter to='#0'>About</LinkFooter>
-            </li>
-            <li>
-              <LinkFooter to='#0'>Our Team</LinkFooter>
-            </li>
-            <li>
-              <LinkFooter to='#0'>Careers</LinkFooter>
-            </li>
-            <li>
-              <LinkFooter to='#0'>Contact</LinkFooter>
-            </li>
-          </ul>
+        <div className={styles.iconsWrapper}>
+          <LinkIcon to='#0' icon={<FacebookIcon />} />
+          <LinkIcon to='#0' icon={<TwitterIcon />} />
+          <LinkIcon to='#0' icon={<PinterestIcon />} />
+          <LinkIcon to='#0' icon={<InstagramIcon />} />
         </div>
       </div>
-      <div className={styles.iconsWrapper}>
-        <LinkIcon to='#0' icon={<FacebookIcon />} />
-        <LinkIcon to='#0' icon={<TwitterIcon />} />
-        <LinkIcon to='#0' icon={<PinterestIcon />} />
-        <LinkIcon to='#0' icon={<InstagramIcon />} />
-      </div>
-    </div>
+    </Container>
   </footer>
 );
 
