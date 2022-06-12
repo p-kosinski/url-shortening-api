0c603a81b3e151599427b8a0ebd86c1158c21c5c
p-kosinski <kosinskipiotr.98@gmail.com>
2022-06-12 15:15:30 +0200

    Add url shortening basic functionality

--- src/components/features/ShortenLinks/ShortenLinks.js
+++ src/components/features/ShortenLinks/ShortenLinks.js
@@ -1,41 +1,66 @@
+import { useState } from 'react';
+
 import ShortenedLink from '../ShortenedLink/ShortenedLink';
 
 import styles from './ShortenLinks.module.scss';
 
-const ShortenLinks = () => (
-  <section className={styles.component}>
-    <form className={styles.form}>
-      <input
-        placeholder='Shorten a link here...'
-        className={styles.input}
-      />
-      <button className={styles.submitButton}>
-        Shorten it!
-      </button>
-    </form>
-    <div className={styles.links}>
-      <ul>
-        <li>
-          <ShortenedLink
-            originalLink='https://www.frontendmentor.io'
-            shortenedLink='https://rel.ink/k4lKyk'
-          />
-        </li>
-        <li>
-          <ShortenedLink
-            originalLink='https://www.twitter.com/frontendmentor'
-            shortenedLink='https://rel.ink/gxOXp9'
-          />
-        </li>
-        <li>
-          <ShortenedLink
-            originalLink='https://www.linkedin.com/company/frontend-mentor'
-            shortenedLink='https://rel.ink/gob3X9'
-          />
-        </li>
-      </ul>
-    </div>
-  </section>
-);
+const ShortenLinks = () => {
+  const [linkToShorten, changeLinkToShorten] = useState('');
+  const [shortenedLinks, changeShortenedLinks] = useState([]);
+
+  const shortenLinkWithAPI = async (link) => {
+    const url = `https://api.shrtco.de/v2/shorten?url=${link}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const { ok, result } = data;
+
+    if(ok) {
+      const linkData = {
+        code: result.code,
+        originalLink: result.original_link,
+        shortLink: result.full_short_link,
+      };
+
+      changeShortenedLinks(currentState => [...currentState, linkData]);
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    shortenLinkWithAPI(linkToShorten);
+  };
+
+  return (
+    <section className={styles.component}>
+      <form className={styles.form}>
+        <input
+          placeholder='Shorten a link here...'
+          className={styles.input}
+          onChange={(event) => changeLinkToShorten(event.target.value)}
+        />
+        <button
+          className={styles.submitButton}
+          onClick={(event) => handleSubmit(event)}
+        >
+          Shorten it!
+        </button>
+      </form>
+      <div className={styles.links}>
+        <ul>
+          {shortenedLinks.map((shortenedLink, i) => (
+            <li key={i}>
+              <ShortenedLink
+                originalLink={shortenedLink.originalLink}
+                shortenedLink={shortenedLink.shortLink}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
 
 export default ShortenLinks;
