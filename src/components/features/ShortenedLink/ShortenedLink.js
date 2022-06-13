385e141479e17ba2b078196ca6fd150177635d9b
p-kosinski <kosinskipiotr.98@gmail.com>
2022-06-13 19:09:31 +0200

    Add copy link to clipboard functionality

--- src/components/features/ShortenedLink/ShortenedLink.js
+++ src/components/features/ShortenedLink/ShortenedLink.js
@@ -1,23 +1,54 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
+import clsx from 'clsx';
 
 import styles from './ShortenedLink.module.scss';
 
-const ShortenedLink = ({ originalLink, shortenedLink }) => (
-  <div className={styles.component}>
-    <div className={styles.originalLink}>
-      <span>{originalLink}</span>
+const ShortenedLink = ({ originalLink, shortenedLink }) => {
+  const [copySuccess, setCopySuccess] = useState(false);
+  const [copyError, setCopyError] = useState(false);
+
+  const copyToClipboard = (text) => {
+    return navigator.clipboard.writeText(text);
+  };
+
+  const handleCopyClick = async () => {
+    setCopySuccess(false);
+    setCopyError(false);
+
+    try {
+      await copyToClipboard(shortenedLink);
+      setCopySuccess(true);
+    } catch (error) {
+      console.error(error);
+      setCopyError(true);
+    }
+  };
+
+  return (
+    <div className={styles.component}>
+      <div className={styles.originalLink}>
+        <span>{originalLink}</span>
+      </div>
+      <hr className={styles.divider} />
+      <div className={styles.shortenedLink}>
+        <span>
+          <mark>{shortenedLink}</mark>
+        </span>
+      </div>
+      <button
+        className={clsx(
+          styles.copyButton,
+          copySuccess && styles.copyButtonSuccess,
+          copyError && styles.copyButtonError
+        )}
+        onClick={() => handleCopyClick()}
+      >
+        {copySuccess ? 'Copied!' : 'Copy'}
+      </button>
     </div>
-    <hr className={styles.divider} />
-    <div className={styles.shortenedLink}>
-      <span>
-        <mark>{shortenedLink}</mark>
-      </span>
-    </div>
-    <button className={styles.copyButton}>
-      Copy
-    </button>
-  </div>
-);
+  );
+};
 
 ShortenedLink.propTypes = {
   originalLink: PropTypes.string.isRequired,
